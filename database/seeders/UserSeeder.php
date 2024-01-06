e5e5dfa8599f922b06781bcf3bc39ea465bcbe5d
Tanvi183 <mneshat7@gmail.com>
2024-01-06 06:18:25 -0800

    user database and seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(DB::table('users')->get()->count() == 0){
+            DB::table('users')->insert([
+                'name' => 'Sana Ullah',
+                'email' => 'mneshat7@gmail.com',
+                'password'=> Hash::make('12345678'),
+                'role' => 'admin',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
